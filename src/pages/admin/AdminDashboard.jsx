1cdbf394f77bff8deaaa5747533e9de3fd97580f
Hamza Abd El-Baset <hamza.abdelbaset.elgebaly@gmail.com>
2024-01-08 10:47:42 +0200

    Admin Page is done, but lacks for tables pages

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,6 +1,13 @@
+import AdminMain from "./AdminMain";
+import AdminSidebabr from "./AdminSidebar";
+import "./admin.css"
+
 const AdminDashboard = () => {
     return ( 
-        <h1>Admin Dashboard Page</h1>
+        <section className="admin-dashboard">
+            <AdminSidebabr/>
+            <AdminMain/>
+        </section>
      );
 }
  
