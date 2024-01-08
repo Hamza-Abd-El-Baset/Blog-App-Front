15b50528dd751e39bcbfaed768fce1ecd0e9280d
Hamza Abd El-Baset <hamza.abdelbaset.elgebaly@gmail.com>
2024-01-08 17:10:57 +0200

    Register Page is almost done, but lacks for empty input validation

--- src/pages/forms/Register.jsx
+++ src/pages/forms/Register.jsx
@@ -1,6 +1,52 @@
+import { Link } from "react-router-dom";
+import "./form.css"
+
 const Register = () => {
     return ( 
-        <h1>Register Page</h1>
+        <section className="form-container">
+            <h1 className="form-title">Create new account</h1>
+            <form className="form">
+                <div className="form-group">
+                    <label htmlFor="username" className="form-label">
+                        Username
+                    </label>
+                    <input
+                        type="text"
+                        className="form-input"
+                        id="username"
+                        placeholder="Enter your username"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email" className="form-label">
+                        Email
+                    </label>
+                    <input
+                        type="text"
+                        className="form-input"
+                        id="email"
+                        placeholder="Enter your email"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password" className="form-label">
+                        Password
+                    </label>
+                    <input
+                        type="text"
+                        className="form-input"
+                        id="password"
+                        placeholder="Enter your password"
+                    />
+                </div>
+                <button className="form-btn" type="submit">
+                    Register
+                </button>
+            </form>
+            <div className="form-footer">
+                Already have an account? <Link to="/login">Login</Link>
+            </div>
+        </section>
      );
 }
  
