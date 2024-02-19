608f3c9bd69c35de387e1bb5f76cee443d6e3922
Hamza Abd-El-Baset <hamza.abdelbaset.elgebaly@gmail.com>
2024-02-19 12:10:29 +0200

    User is now redirected to login afterregistering

--- src/redux/apiCalls/authApiCall.js
+++ src/redux/apiCalls/authApiCall.js
@@ -8,12 +8,9 @@ export function loginUser(user) {
     return async (dispatch) => {
         try {
             const {data} = await request.post('/api/auth/login', user)
-
             dispatch(authActions.login(data))
-            localStorage.setItem("userInfo", JSON.stringify(data))
         } catch (error) {
             toast.error(error.response.data.message)
-            console.log(error)
         }
     }
 }
@@ -24,4 +21,19 @@ export function logoutUser(user) {
         dispatch(authActions.logout())
         localStorage.removeItem("userInfo")
 }
+}
+
+
+//Register User
+export function registerUser(user) {
+    return async (dispatch) => {
+        try {
+            const {data} = await request.post('/api/auth/register', user)
+
+            dispatch(authActions.register(data.message))
+            localStorage.setItem("userInfo", JSON.stringify(data))
+        } catch (error) {
+            toast.error(error.response.data.message)
+        }
+    }
 }
